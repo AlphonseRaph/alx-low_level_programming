30671ff780b96317af7398d0024b155ed6cc2c5f
AlphonseRaph <alphonseoketch2004@gmail.com>
2022-09-09 07:56:27 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,10 +5,11 @@
  *
  * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
 	char Mystr[] = "\"Programming is like building a multilingual puzzle";
-	puts (Mystr);
+
+	puts(Mystr);
 
 	return (0);
 } /*My first C programme*/
