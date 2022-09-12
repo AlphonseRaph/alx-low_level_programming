afa9985308d396d46d5a0bf5f0e958dbdd0f64d0
AlphonseRaph <alphonseoketch2004@gmail.com>
2022-09-12 02:32:15 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Print numbers 1 to 10
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 1; ch <= 10; ch++)
+		printf("%d\n", ch);
+	return (0);
+}
