cff95b3d1467323cb482354f8f289bccaa3fed71
AlphonseRaph <alphonseoketch2004@gmail.com>
2022-09-13 03:26:38 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Print _putchar
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ch;
+
+	ch = '_';
+	putchar(ch);
+	ch = 'p';
+	putchar(ch);
+	ch = 'u';
+	putchar(ch);
+	ch = 't';
+	putchar(ch);
+	ch = 'c';
+	putchar(ch);
+	ch = 'h';
+	putchar(ch);
+	ch = 'a';
+	putchar(ch);
+	ch = 'r';
+	putchar(ch);
+	putchar('\n')
+	return (0);
+}
+
