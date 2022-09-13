590a6f7983f5d428405271ef952a9502d999f1f4
AlphonseRaph <alphonseoketch2004@gmail.com>
2022-09-13 08:03:33 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: Print alphabet letters
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ch;
+	for(ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch)
+	putchar('\n')
+	return (0)
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,2 +1,3 @@
 /*Header files go here*/
 int _putchar(char ch);
+void print_alphabet(void);
