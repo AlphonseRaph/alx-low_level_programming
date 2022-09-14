6ff5ae400dd47acae630e362cf91b5b810a363bd
AlphonseRaph <alphonseoketch2004@gmail.com>
2022-09-14 09:32:01 -0700

    Task 4

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,11 @@
 /*Header files go here*/
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
 int _putchar(char ch);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
+
+#endif
