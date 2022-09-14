dadd4b7a60206256abbf6e972182784735ead8c0
AlphonseRaph <alphonseoketch2004@gmail.com>
2022-09-14 10:00:10 -0700

    Task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_sign - Print sign of a number
+ *
+ * @n: number
+ *
+ * Return: 1 if n > 0, 0 if n = 0, -1 if n < 0)
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+		return (1);
+	if (n = 0)
+		return (0);
+	if (n < 0)
+		return (-1);
+}
