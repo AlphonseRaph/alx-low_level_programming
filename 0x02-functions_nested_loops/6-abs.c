279ed9bbedad1cfa87ed1df194f9241e49a26fe5
AlphonseRaph <alphonseoketch2004@gmail.com>
2022-09-15 09:21:23 -0700

    Task 6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,12 +2,17 @@
 
 /**
  * _abs - Entry point
+ *
+ * @c - parameter to be checked
+ *
  * Return: Always 0 (Success)
  */
 
 int _abs(int c)
 {
 	if (c > 0)
-		c = c* -1;
+	{
+		return(c * -1);
+	}
 	return (c);
 }
