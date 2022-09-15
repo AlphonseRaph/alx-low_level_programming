96e6b7ab664901e74d06005402c08e16c9e7f6b1
AlphonseRaph <alphonseoketch2004@gmail.com>
2022-09-15 09:45:33 -0700

    Task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Print last digit
+ *
+ * @c - To be checked
+ *
+ * Return: Always 0.
+ */
+
+int print_last_digit(int c);
+{
+	int LD;
+
+	LD = c % 10;
+	return (LD);
+}
