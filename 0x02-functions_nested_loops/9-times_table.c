27fbfefd6337e2e1f518622c35672e031b7343c5
AlphonseRaph <alphonseoketch2004@gmail.com>
2022-09-16 07:15:36 -0700

    Task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * times_table - Print times table
+ *
+ * Return: no return value
+ */
+
+void times_table(void)
+{
+	int a, b, c;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b < 10; b++)
+		{
+			C = a * b;
+			if (c / 10 != 0)
+			{
+				if (b != 0)
+				{
+					_putchar (',');
+					_putchar (' ');
+				}
+				_putchar (c / 10 + '0'');
+				_putchar (c / 10 + '0');
+			}
+			else
+			{
+				if (b != 0)
+				{
+					_putchar (', ');
+					_putchar (' ');
+					_putchar (' ');
+				}
+				_putchar (c + '0');
+			}
+		}
+	_putchar('\n');
+	}
+}
