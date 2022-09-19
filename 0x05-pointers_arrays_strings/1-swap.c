4d672abd829198eb523554b16bfd3ac554a8f8f8
AlphonseRaph <alphonseoketch2004@gmail.com>
2022-09-19 11:23:30 -0700

    Task 1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -12,11 +12,11 @@
 
 void swap_int(int *a, int *b)
 {
-	int INT_MAX;
-	int INT_MIN;
+	int *INT_MAX;
+	int *INT_MIN;
 
-	INT_MAX = 1024;
+	*INT_MAX = 1024;
 	*a = 98;
 	*b = 42;
-	INT_MAX = -4096;
+	*INT_MAX = -4096;
 }
