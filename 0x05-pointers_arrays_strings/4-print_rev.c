4923b3f38e07300e408527119f5efbca2509a888
AlphonseRaph <alphonseoketch2004@gmail.com>
2022-09-19 19:49:00 -0700

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_rev - Print 
+ *
+ * @leng: int 1
+ * @g: int 2
+ *
+ * Return: Nothing
+ */
+
+void print_rev(char *s);
+{
+	int leng g;
+
+	leng = strlen(*s);
+	for(g = leng-1; g >= 0; g--)
+		putchar(*s[g]);
+	putchar('\n');
+}
