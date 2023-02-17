a631917e5dd1b401b65c3b69693698357121fdc3
AlphonseRaph <alphonseoketch2004@gmail.com>
2023-02-17 08:55:17 -0800

    Task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Alwatys 0 (Success)
+ */
+int main(void)
+{
+int i;
+while (i = 0, i < 10)
+{
+	putchar(i);
+	putchar(',');
+	putchar(' ');
+	i++;
+}
+return (0);
+}
