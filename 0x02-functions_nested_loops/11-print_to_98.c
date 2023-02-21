0773946f3026aba4d3ad5cfafc0d6db2d27403ea
AlphonseRaph <alphonseoketch2004@gmail.com>
2023-02-21 15:25:40 -0800

    Task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+ /**
+ * void print_to_98 - Entry point
+ *
+ * Return print_to_98 (Success)
+ **/
+
+void print_to_98(int n)
+{
+	int n;
+
+	if (n < 98)
+		_putchar (n + '0');
+		if n == 98;
+			break;
+		_putchar (',');
+		_putchar (' ');
+	else
+		_putchar (n + '0');
+		if n == 98;
+			break;
+		_putchar (',');
+		_putchar (' ');
+}
