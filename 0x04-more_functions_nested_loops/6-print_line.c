5e3eaaa9b47c154300a3f979c3c8399b490af347
AlphonseRaph <alphonseoketch2004@gmail.com>
2023-02-24 00:34:49 -0800

    Task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,16 +9,17 @@
 void print_line(int n)
 {
 	int i;
-	for(i = 0; i <= n; i++)
+
+	for (i = 0; i <= n; i++)
 	{
 		if (n <= 0)
-			{
-				_putchar('\n');
-			}
+		{
+			_putchar('\n');
+		}
 		else
-			{
-				_putchar('_');
-			}
+		{
+			_putchar('_');
+		}
 	}
 	_putchar('\n');
 }
