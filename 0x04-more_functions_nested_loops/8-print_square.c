8125650a426fe27f75c66e158c4a4070d1839e76
AlphonseRaph <alphonseoketch2004@gmail.com>
2023-02-24 09:36:39 -0800

    Task 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -13,15 +13,18 @@ void print_square(int size)
 	int i, z;
 
 	if (size <= 0)
+	{
 		_putchar('\n');
+	}
 	else
-
+	{
 		for (i = 0; i < size; i++)
 		{
 			for (z = 0; z < size; z++)
 			{
 				_putchar('#');
 			}
-		_putchar('\n');
+			_putchar('\n');
 		}
+	}
 }
