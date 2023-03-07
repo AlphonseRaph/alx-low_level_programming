d77a1f1d0c0395c75a171eb2c07d7fd265eb0088
AlphonseRaph <alphonseoketch2004@gmail.com>
2023-03-07 08:59:32 -0800

    Task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,21 +1,31 @@
+#include "main.h"
+
+/**
+ * int _strspn - gets the length of a prefix substring
+ *
+ * @s: segment
+ * @accept: accept bytes
+ * Return: s
+ */
+
 unsigned int _strspn(char *s, char *accept)
 {
-    unsigned int byte = 0;
-    int index;
+	unsigned int byte = 0;
+	int index;
 
-    while(*s)
-    {
-        for(index = 0;accept[index];index++)
-        {
-            if (*s == accept[index])
-            {
-                byte++;
-                break;
-            }
-            else if (accept[index] == '\0')
-                return (byte);
-        }
-        s++;
-    }
-        return (byte);
+	while (*s)
+	{
+		for (index = 0; accept[index]; index++)
+		{
+			if (*s == accept[index])
+			{
+				byte++;
+				break;
+			}
+			else if (accept[index] == '\0')
+				return (byte);
+		}
+		s++;
+	}
+	return (byte);
 }
