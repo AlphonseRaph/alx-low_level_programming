a5ac594a93350170ab9d8b1aa10ff39b618d4627
AlphonseRaph <alphonseoketch2004@gmail.com>
2023-03-10 17:39:53 -0800

    Task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,13 +2,13 @@
 #include <stdio.h>
 
 /**
- * main -
- * 
+ * main - function
+ *
  * @argv - array containing variables
  * Return: 0
  */
 
-int main (int argc __attribute__((unused)), char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
