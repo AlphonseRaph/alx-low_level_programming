1f2d92dd65a70c229687683af2236e7baf1ab34f
AlphonseRaph <alphonseoketch2004@gmail.com>
2023-03-11 00:04:30 -0800

    Static library

---
+++ 0x09-static_libraries/2-strncpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strncpy - copystring
+ *
+ * @dest: destination
+ * @src: source
+ * @n: int
+ * Return: char
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+
+}
