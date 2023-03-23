0753795060d41ace0111ecd32f153171ff146a81
AlphonseRaph <alphonseoketch2004@gmail.com>
2023-03-22 18:18:55 -0700

    Task 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an integer
+ *
+ * @array: array
+ * @size: size of array
+ * @action: pointer to function
+ */
+ void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	for(i= 0; i < size; i++)
+		action(array[i]);
+}
