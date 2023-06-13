0ffa918389cee6b3e50a1ffc661345f1ad743704
AlphonseRaph <alphonseoketch2004@gamil.com>
2023-06-13 21:46:15 +0300

    Task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,6 +14,7 @@
 char *create_array(unsigned int size, char c)
 {
 	char *p;
+	unsigned int i;
 
 	if (size == 0)
 	{
@@ -24,5 +25,7 @@ char *create_array(unsigned int size, char c)
 	{
 		return (NULL);
 	}
+	for (i = 0; i < size; i++)
+		p[i] = c;
 	return (p);
 }
