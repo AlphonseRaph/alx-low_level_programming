46ffca2a7a211f972d4ff2789c784bab44770d53
AlphonseRaph <alphonseoketch2004@gmail.com>
2023-06-30 21:25:42 +0300

    Frees linked list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list
+ *
+ * @head: pointer to struct
+ *
+ * Return: nothing
+ */
+
+void free_list(list_t *head)
+{
+	list_t *temp;
+	
+	temp = head;
+
+	while (temp != NULL)
+	{
+		free(temp->str);
+		free(temp);
+		temp = temp->next;
+	}
+}
