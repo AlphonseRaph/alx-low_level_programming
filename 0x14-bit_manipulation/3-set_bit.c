599e74e1df57687840936118448198078f8373fe
AlphinseRaph <alphonseoketch2004@gmail.com>
2023-11-23 21:56:08 +0300

    Set bit to 1

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/** set_bit - sets bit to 1
+ * 
+ * @n: long int
+ * @index: index of bit
+ *
+ * Return: 1 or -1
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int i = 1;
+	unsigned int x = 0;
+
+	for (; x < index; x++)
+	{
+		i = i << 1;
+	}
+	*n = *n | i;
+	return (*n);
+}
