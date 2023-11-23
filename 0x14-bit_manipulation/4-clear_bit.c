a530b19654a95a455712fa2fd3ebe689173fa860
AlphinseRaph <alphonseoketch2004@gmail.com>
2023-11-23 21:56:35 +0300

    Set bit to 0

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * clear_bit - set bit to 0
+ *
+ * @n: long int
+ * @index: index of bit
+ *
+ * Return: 1 0r -1
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int i = 1;
+
+	i = i << index;
+	i = ~i;
+	*n = *n & i;
+	return (*n);
+}
+
