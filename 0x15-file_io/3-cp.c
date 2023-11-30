3fb59e53c3dd681ee6893e96f4f6a3fb27fc27b8
AlphinseRaph <alphonseoketch2004@gmail.com>
2023-11-30 04:33:30 +0300

    Cp FILE_FROM FILE_TO

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,45 +3,57 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-int main( char *file_from, int argc, char *argv[])
+int main( int argc, char *argv[])
 {
 	int fd = 0;
-	char buffer = malloc(1024);
+	char *buffer = malloc(1024);
 	ssize_t x = 0;
 	int fp = 0;
 	ssize_t y = 0;
+	char *file_from;
+	char *file_to;
 
-	if (argc < 4)
+	if (argc < 3)
 	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
-		dprintf("Usage: cp file_from file_to\n");
 	}
-	if (file_from == NULL)
-	{
-		exit(98);
-		dprintf("Error: Can't read from file argv[0]\n");
-	}
+	file_from = argv[1];
+	file_to = argv[2];
 	fd = open(file_from, O_RDONLY);
+	if (fd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		exit(98);
+	}
 	x = read(fd, buffer, 1024);
+	if (x == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		exit(98);
+	}
 	if (close(fd) == -1)
 	{
-		dprintf("Error: Can't close fd %d\n", fd);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
 	}
 	fp = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 664);
+	if (fp == -1)
+	{
+
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+		exit(99);
+	}
 	y = write(fp, buffer, 1024);
 	if (y == -1)
 	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
 		exit(99);
-		dprintf("Error: Can't write to argv[1]\n");
 	}
 	if (close(fp) == -1)
 	{
-		dprintf("Error: Can't close fd %d\n", fd);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fp);
+		exit(100);
 	}
 	return (0);
 }
-
-
-
-
-
