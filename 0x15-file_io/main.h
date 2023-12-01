bccecc18ad348475c8726a3501b51d2e04a5c9cc
AlphinseRaph <alphonseoketch2004@gmail.com>
2023-12-01 04:01:33 +0300

    Header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,10 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
+#endif
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 #if defined(__LP64__)
-#define ELfW(type) El64_ ## type
+#define ElfW(type) Elf64_ ## type
 #else
 #define ElfW(type) Elf32_ ## type
 #endif
