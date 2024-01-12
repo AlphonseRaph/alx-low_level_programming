ce52200b7526a44a348b7084a72847fb2f82ba49
AlphonseRaph <alphonseoketch2004@gmail.com>
2024-01-11 16:56:52 -0800

    returns the nth node of a list

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -7,14 +7,19 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	unsigned int i = 0;
 	dlistint_t *temp = head;
 
-	if (head->next == NULL)
+	while (temp->next != NULL && i <= index)
 	{
-		return (NULL);
-	}
-	else if (i <= index)
-	{
-		temp = temp->next;
-		i++;
+		if (temp->next == NULL && i == index)
+			return (NULL);
+		else if (i == index && temp->next != NULL)
+		{
+			return (temp);
+		}
+		else
+		{
+			temp = temp->next;
+			i++;
+		}
 	}
 	return (temp);
 }
